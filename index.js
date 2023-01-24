d2ca3b8b6194fd15ffeb69a8e9c42fb0118c4cac
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2023-01-24 22:39:14 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const question = document.querySelector('.question');
+  const yesBtn = document.querySelector('.yes-btn');
+ yesBtn.addEventListener('click', () => {
+   question.innerHTML = 'I Love You Too :)';
+});
+//sets a random absolute position to a html element; receives the html element
+function moveElmRand(elm){
+ elm.style.position ='absolute';
+ elm.style.top = Math.floor(Math.random()*90+5)+'%';
+ elm.style.left = Math.floor(Math.random()*90+5)+'%';
+}
+
+//get the #btn_test
+var btn_test = document.querySelector('#btn_test');
+
+//register to call moveElmRand() on mouseenter event to #btn_test
+btn_test.addEventListener('mouseenter', function(e){ moveElmRand(e.target);});
+
+//register click to #btn_test
+btn_test.addEventListener('click', function(e){ alert('You are Good.');});
